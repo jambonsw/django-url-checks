dc72154f2bf5f11876fb79e82a862ccea9efe923
Andrew Pinkham <code@andrewsforge.com>
2018-12-18 17:46:11 +0100

    ➕🔧 pytest-django

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+"""PyTest Configuration"""
+from django.conf import settings
+
+
+def pytest_configure():
+    """Configure Django settings"""
+    settings.configure(
+        SITE_ID=1,
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": ":memory:",
+            }
+        },
+        INSTALLED_APPS=[
+            "django.contrib.admin",
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.messages",
+            "django.contrib.sessions",
+            "django.contrib.sites",
+        ],
+        MIDDLEWARE=[
+            "django.contrib.sessions.middleware.SessionMiddleware",
+            "django.contrib.auth.middleware.AuthenticationMiddleware",
+            "django.contrib.messages.middleware.MessageMiddleware",
+        ],
+    )
